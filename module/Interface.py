bf5a41cba8f0454a13118a693ef45ab698601ecf
ValentinAllard <valentin.allard@rocketmail.com>
2020-04-07 16:23:12 +0200

    Imported calculator which was modified to account power operation

---
+++ module/Interface.py
@@ -0,0 +1,1 @@
+import Material_law as ml
--- module/Material_law.py
+++ module/Material_law.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import Calculator as calc
 
 dirname = os.path.dirname(__file__)
 
@@ -7,12 +8,13 @@ material_data_path = os.path.join(dirname, "data/Bone_Material_Law.xlsx")
 
 
 class MaterialLaw:
-    def __init__(self, name, site, measure, law, range_measure):
+    def __init__(self, name, site, measure, law, range_measure, speed_rate):
         self. name = name
         self.site = site
         self.measure = measure
         self.law = law
         self.range_measure = range_measure
+        self.speed_rate = speed_rate
 
     def modify_name(self, new_name):
         self.name = new_name
@@ -29,24 +31,35 @@ class MaterialLaw:
     def modify_range(self, new_range):
         self.range_measure = new_range
 
+    def get_law(self, rate=0):
+        self_measure = self.measure
+
+        def fun(density):
+            formula = self.law.split('=')[-1]
+            _vars = {self_measure: density, 'eps_dev': rate}
+            return calc.evaluate(formula, _vars)
+
+        return fun
+
 
 class MaterialData:
     def __init__(self, path):
         self.path = path
         self.data = pd.read_excel(self.path)
         self.values = self.data.values
-        self.columns = self.data.values
+        self.columns = self.data.columns
         self.index = self.data.index
         self.number_material_laws = self.index.stop - self.index.start
         self.material_laws = []
-        for i in range(index.stop - index.start):
+        for i in range(self.index.stop - self.index.start):
             name = self.values[i][0]
             site = self.values[i][1]
             measure = self.values[i][3]
             law = self.values[i][5]
             range_measure = [float(x) for x in self.values[i][4].split('-')]
             range_measure.sort()
-            self.material_laws.append(MaterialLaw(name, site, measure, law, range_measure))
+            speed_rate = [float(_x) for _x in self.values[i][7].split('-')]
+            self.material_laws.append(MaterialLaw(name, site, measure, law, range_measure, speed_rate))
 
     def add_material_law(self, new_material_law):
         if type(new_material_law) == MaterialLaw:
@@ -70,13 +83,7 @@ class MaterialData:
             return 'Type is not MaterialLaw'
 
 
-data = pd.read_excel(material_data_path)
-values = data.values
-columns = data.columns
-index = data.index
-laws = MaterialData(material_data_path)
 
-print(columns)
 
 
 
